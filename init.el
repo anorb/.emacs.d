4d19824f1bfbadd6df8daaacdb8ffb699a171e60
anorb <austin@norb.xyz>
2018-07-16 20:52:56 -0700

    Move functions to their own file
    - Add snippet for code block in org
    - Rename elisp.d -> lisp

--- init.el
+++ init.el
@@ -91,8 +91,8 @@
 			   (name . "^\\*Messages\\*$")
                            (name . "^\\*Help\\*$"))))))
 
-
-
+;;; Load misc-defuns for keybinds
+(load "~/.emacs.d/lisp/misc-defuns.el")
 
 ;;; Custom keybinds
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -433,86 +433,7 @@
 
 
 ;;; Load private config
-(load "~/.emacs.d/elisp.d/private.el")
-
-;;; Various functions used in keybinds/hooks
-(defun kill-dired-buffers ()
-  "Kill all opened dired buffers."
-  (interactive)
-  (mapc (lambda (buffer)
-          (when (eq 'dired-mode (buffer-local-value 'major-mode buffer))
-            (kill-buffer buffer)))
-        (buffer-list)))
-
-;; Dealing with window scrolling
-(defun window-half-height ()
-  "Return 50% of the window height for scrolling by half."
-  (max 1 (/ (1- (window-height (selected-window))) 2)))
-
-(defun scroll-up-half ()
-  "Scroll up the page by 50%."
-  (interactive)
-  (scroll-up (window-half-height)))
-
-(defun scroll-down-half ()
-  "Scroll down the page by 50%."
-  (interactive)
-  (scroll-down (window-half-height)))
-
-;; Cut & copy lines when cursor is over them
-(defun xah-copy-line-or-region ()
-  "Copy current line, or text selection.
-When called repeatedly, append copy subsequent lines.
-When `universal-argument' is called first, copy whole
-buffer (respects `narrow-to-region').
-
-URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
-Version 2017-12-04"
-  (interactive)
-  (if current-prefix-arg
-      (progn
-        (kill-ring-save (point-min) (point-max)))
-    (if (use-region-p)
-        (progn
-          (kill-ring-save (region-beginning) (region-end)))
-      (if (eq last-command this-command)
-          (if (eobp)
-              (progn )
-            (progn
-              (kill-append "\n" nil)
-              (kill-append
-               (buffer-substring-no-properties (line-beginning-position) (line-end-position))
-               nil)
-              (progn
-                (end-of-line)
-                (forward-char))))
-        (if (eobp)
-            (if (eq (char-before) 10 )
-                (progn )
-              (progn
-                (kill-ring-save (line-beginning-position) (line-end-position))
-                (end-of-line)))
-          (progn
-            (kill-ring-save (line-beginning-position) (line-end-position))
-            (end-of-line)
-            (forward-char)))))))
-
-
-(defun xah-cut-line-or-region ()
-  "Cut current line, or text selection.
-When `universal-argument' is called first, cut whole
-buffer (respects `narrow-to-region').
-
-URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
-Version 2015-06-10"
-  (interactive)
-  (if current-prefix-arg
-      (progn ; not using kill-region because we don't want to include previous kill
-        (kill-new (buffer-string))
-        (delete-region (point-min) (point-max)))
-    (progn (if (use-region-p)
-               (kill-region (region-beginning) (region-end) t)
-             (kill-region (line-beginning-position) (line-beginning-position 2))))))
+(load "~/.emacs.d/lisp/private.el")
 
 (provide 'init)
 ;;; init.el ends here
